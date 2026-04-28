75b0d52759c361e4fb50c4599df3c9a4515555e2
richKirl <162905102+richKirl@users.noreply.github.com>
2026-04-28 21:43:26 +0930

    Add files via upload

---
+++ src/basestate.rs
@@ -0,0 +1,47 @@
+use math3d::{mat4vf::Mat4vf, vec4f::Vec4f};
+use mxg11l::{GlDepthExt, GlFunctions, GlVsyncExt, GlWindow, XDisplay};
+
+use crate::basetoggles::BaseToggles;
+
+pub struct BaseState<'a> {
+    pub window: GlWindow<'a>, // Теперь окно знает, что дисплей живет снаружи
+    pub window_config: Vec4f,
+    pub perspective_config: Vec4f,
+    pub projection: Mat4vf,
+    pub background: Vec4f,
+    pub basetoggles: BaseToggles,
+    pub gl: &'a GlFunctions,
+}
+
+impl<'a> BaseState<'a> {
+    pub fn new(display: &'a XDisplay, gl: &'a GlFunctions) -> Self {
+        let w = 800.0;
+        let h = 600.0;
+
+        let window_config = Vec4f::new(w, h, w * 0.5, h * 0.5);
+        let perspective_config = Vec4f::new(45.0f32.to_radians(), w / h, 0.1, 1000.0);
+        let projection = Mat4vf::perspective(
+            perspective_config.fov(),
+            perspective_config.aspect(),
+            perspective_config.near(),
+            perspective_config.far(),
+        );
+
+        // Передаем ссылку на display, который потом переместим в структуру
+        let window = GlWindow::new(&display, "Test Rust Library", w as u32, h as u32, 4, 6)
+            .expect("Не удалось открыть window")
+            .add_vsync(&gl, true)
+            .add_depth_with_alpha();
+
+        Self {
+            // display, // Сохраняем здесь!
+            window,
+            window_config,
+            perspective_config,
+            projection,
+            background: Vec4f::new(0.22, 0.44, 0.66, 1.0),
+            basetoggles: BaseToggles::new(),
+            gl: gl,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #[allow(non_snake_case, unused)]
 use math3d::{dualquatf, frustum, mat4vf, quatf, vec3d, vec3f, vec4d, vec4f};
-use math3d::{mat4vf::Mat4vf, quatf::Quatf, vec3f::Vec3f, vec4f::Vec4f};
-use mxg11l::{GlDepthExt, GlFunctions, GlVsyncExt, GlWindow, Timer, XDisplay};
+use math3d::{quatf::Quatf, vec3f::Vec3f};
+use mxg11l::{GlFunctions, Timer, XDisplay};
 use mxgimage::TgaImage;
 #[allow(non_snake_case)]
 mod autoMesh;
+mod basestate;
 mod basetoggles;
 mod camera;
 mod input_handle;
@@ -13,55 +14,13 @@ mod shaders;
 mod transform;
 use crate::{
     autoMesh::auto_cube::Cube,
-    basetoggles::BaseToggles,
+    basestate::BaseState,
     camera::Camera,
     input_handle::InputState,
     shader::Shader,
     shaders::{FRAG_SRC, VERT_SRC},
     transform::Transformer,
 };
-pub struct BaseState<'a> {
-    pub window: GlWindow<'a>, // Теперь окно знает, что дисплей живет снаружи
-    pub window_config: Vec4f,
-    pub perspective_config: Vec4f,
-    pub projection: Mat4vf,
-    pub background: Vec4f,
-    pub basetoggles: BaseToggles,
-    gl: &'a GlFunctions,
-}
-
-impl<'a> BaseState<'a> {
-    pub fn new(display: &'a XDisplay, gl: &'a GlFunctions) -> Self {
-        let w = 800.0;
-        let h = 600.0;
-
-        let window_config = Vec4f::new(w, h, w * 0.5, h * 0.5);
-        let perspective_config = Vec4f::new(45.0f32.to_radians(), w / h, 0.1, 1000.0);
-        let projection = Mat4vf::perspective(
-            perspective_config.fov(),
-            perspective_config.aspect(),
-            perspective_config.near(),
-            perspective_config.far(),
-        );
-
-        // Передаем ссылку на display, который потом переместим в структуру
-        let window = GlWindow::new(&display, "Test Rust Library", w as u32, h as u32, 4, 6)
-            .expect("Не удалось открыть window")
-            .add_vsync(&gl, true)
-            .add_depth_with_alpha();
-
-        Self {
-            // display, // Сохраняем здесь!
-            window,
-            window_config,
-            perspective_config,
-            projection,
-            background: Vec4f::new(0.22, 0.44, 0.66, 1.0),
-            basetoggles: BaseToggles::new(),
-            gl: gl,
-        }
-    }
-}
 
 fn main() {
     let gl = GlFunctions::load();
